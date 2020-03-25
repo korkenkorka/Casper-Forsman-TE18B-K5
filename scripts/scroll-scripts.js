0a870062aaad25a694b343ca53068db54a9a28c7
Crisper <46033957+korkenkorka@users.noreply.github.com>
2020-03-25 21:54:39 +0100

    Add background text and BETA projects layout

--- scripts/scroll-scripts.js
+++ scripts/scroll-scripts.js
@@ -2,11 +2,17 @@ window.addEventListener("scroll", () => {
     if (window.scrollY > 20) {
         document.querySelector('div[id=top-menu]').style.backgroundColor = '#000E25';
         document.querySelector('div[id=top-menu]').style.top = 0 - window.innerHeight / 30 + "px";
-
-        console.log("test");
+    }
+    else if (window.scrollY < 20 ) {
+        document.querySelector('div[id=top-menu]').style.backgroundColor = 'rgb(0,0,0,0)';
+        document.querySelector('div[id=top-menu]').style.top = 0 + "px";
     }
 
-    if (window.scrollY > window.innerHeight - 1) {
+    if (window.scrollY > window.innerHeight - window.innerHeight / 10) {
         document.querySelector('div[id=top-menu]').style.top = 0 - window.innerHeight / 2 + "px";
     }
 });
+
+document.querySelector('div[id=background-text]').style.marginLeft = 30 + "%";
+
+document.querySelector('div[id=main-content-holder]').style.boxShadow = "0px -15vh 20vh " + window.innerHeight / 30 + "vh #000E25";
