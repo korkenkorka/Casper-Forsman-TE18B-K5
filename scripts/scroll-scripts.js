f0e34dc00b6260a7adb99a71acf234e059ace184
Crisper <46033957+korkenkorka@users.noreply.github.com>
2020-03-26 16:15:33 +0100

    Implement complete projects list

--- scripts/scroll-scripts.js
+++ scripts/scroll-scripts.js
@@ -15,4 +15,4 @@ window.addEventListener("scroll", () => {
 
 document.querySelector('div[id=background-text]').style.marginLeft = 30 + "%";
 
-document.querySelector('div[id=main-content-holder]').style.boxShadow = "0px -15vh 20vh " + window.innerHeight / 30 + "vh #000E25";
+document.querySelector('div[id=main-content-holder]').style.boxShadow = "0px -15vh 20vh " + window.innerHeight / 25 + "vh #000E25";
